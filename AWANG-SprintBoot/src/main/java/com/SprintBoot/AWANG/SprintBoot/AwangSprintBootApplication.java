f0b7f862bea70ac0dc2a79b2148324fb8f903c34
Aaron Lao <91438480+aajmlao@users.noreply.github.com>
2024-09-14 19:29:06 -0700

    add sprintboot

---
+++ AWANG-SprintBoot/src/main/java/com/SprintBoot/AWANG/SprintBoot/AwangSprintBootApplication.java
@@ -0,0 +1,13 @@
+package com.SprintBoot.AWANG.SprintBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwangSprintBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwangSprintBootApplication.class, args);
+	}
+
+}
---
+++ AWANG-SprintBoot/src/test/java/com/SprintBoot/AWANG/SprintBoot/AwangSprintBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SprintBoot.AWANG.SprintBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwangSprintBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

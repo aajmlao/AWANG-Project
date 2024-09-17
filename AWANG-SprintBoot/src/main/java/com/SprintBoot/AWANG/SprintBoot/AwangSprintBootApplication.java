1c630a59a5c402acfd8c00eb0d1a34654205ec96
Aaron Lao <91438480+aajmlao@users.noreply.github.com>
2024-09-16 17:19:15 -0700

    test spring boot and langchain4j integration

--- AWANG-SprintBoot/src/main/java/com/SprintBoot/AWANG/SprintBoot/AwangSprintBootApplication.java
+++ AWANG-SprintBoot/src/main/java/com/SprintBoot/AWANG/SprintBoot/AwangSprintBootApplication.java
@@ -2,8 +2,10 @@ package com.SprintBoot.AWANG.SprintBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@Component
 public class AwangSprintBootApplication {
 
 	public static void main(String[] args) {
